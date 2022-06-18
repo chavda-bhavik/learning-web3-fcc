bc0223c4509a85bd523638fe061336eb36ee1322
chavda-bhavik <bhavik.chvda@gmail.com>
2022-06-18 06:40:06 +0530

    added tests with gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 require('@nomiclabs/hardhat-etherscan');
 require('./tasks/block-number');
+require("hardhat-gas-reporter");
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -11,26 +12,32 @@ require('./tasks/block-number');
  */
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [
-        PRIVATE_KEY
-      ],
-      chainId: 4
-    },
-    localhost: {
-      url: 'http://127.0.0.1:8545/',
-      chainId: 31337
-      // accounts: [..]
-    }
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY
-  },
-  solidity: "0.8.4",
+	defaultNetwork: "hardhat",
+	networks: {
+		rinkeby: {
+			url: RINKEBY_RPC_URL,
+			accounts: [PRIVATE_KEY],
+			chainId: 4,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545/",
+			chainId: 31337,
+			// accounts: [..]
+		},
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+	solidity: "0.8.4",
+	gasReporter: {
+		enabled: true,
+		outputFile: "gas-report.txt",
+		noColors: true,
+		currency: "INR",
+		coinmarketcap: COINMARKETCAP_KEY,
+	},
 };
